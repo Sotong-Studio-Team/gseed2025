e832c09ba3ae3f93efb462081ed2850aaca78cac
erlanggasbr <sembiringerlangga@gmail.com>
2025-07-29 15:26:42 +0700

    Player Movement
    Move with horizontal & vertical input
    Flip x sprite

---
+++ Assets/Script/Character Action/PlayerMovementView.cs
@@ -0,0 +1,59 @@
+using NaughtyAttributes;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovementView : MonoBehaviour
+{
+    public Action OnUpdate;
+
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _sprite;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        OnUpdate?.Invoke();
+    }
+
+    public void MovePlayer(float moveSpeed, Vector2 movement)
+    {
+        _rb.MovePosition(_rb.position + moveSpeed * Time.fixedDeltaTime * movement.normalized);
+    }
+
+    public void AnimatePlayer(Vector2 lastDirection, Vector2 movement)
+    {
+        bool isWalking;
+
+        //set parameter idle animation player
+        _animator.SetFloat("dirX", lastDirection.x);
+        _animator.SetFloat("dirY", lastDirection.y);
+
+        //set parameter walk animation player
+        _animator.SetFloat("moveX", movement.x);
+        _animator.SetFloat("moveY", movement.y);
+
+        if (movement != Vector2.zero)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+        _animator.SetBool("isWalking", isWalking);
+    }
+
+    public void FlipSprite(bool isFlipped)
+    {
+        _sprite.flipX = isFlipped;
+    }
+}
