f0ae154a1e253fa837654edd16b76485bfadcc89
Vindrax-Dev <159908831+Vindrax-Dev@users.noreply.github.com>
2025-07-30 18:44:25 +0700

    commit enemy roaming
    disini deteksinya pakai raycast

---
+++ Assets/Script/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SotongStudio.Bomber
+{
+    public class Enemy
+    {
+        public string enemyName = "Chaser";
+        public float moveSpeed = 2.5f;
+        public int maxHP = 1;
+        public float cellSize = 1f;
+    }
+}
