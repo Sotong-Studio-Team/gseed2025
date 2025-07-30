f2b3aba04647d9c637f3a253e67e506b73b7be8a
SotongCoding <develop.sotong@gmail.com>
2025-07-30 20:45:18 +0700

    Adding set hunt state

--- Assets/Script/Chase Behaviour/HuntBehaviourLogic.cs
+++ Assets/Script/Chase Behaviour/HuntBehaviourLogic.cs
@@ -9,6 +9,7 @@ namespace SotongStudio.Bomber.Gameplay.Enemy.Behaviour
         public readonly IHuntBehaviourComponent _huntComponent;
         private CancellationTokenSource _huntCts;
         private IHuntTarget _currentTarget;
+        private bool _isCanHunt;
 
         public HuntBehaviourLogic(IHuntBehaviourComponent chaseLogic)
         {
@@ -18,8 +19,34 @@ namespace SotongStudio.Bomber.Gameplay.Enemy.Behaviour
             _huntComponent.OnLostTarget.AddListener(OnLostTarget);
         }
 
+        public void SetCanHunt(bool isCanHunt)
+        {
+            _isCanHunt = isCanHunt;
+
+            if (!_isCanHunt)
+            {
+                CancelHuntProcess();
+                _currentTarget = null;
+                _huntComponent.NavAgentHandled.ResetPath();
+            }
+            else
+            {
+                if (_currentTarget == null) { return; }
+
+                CancelHuntProcess();
+                _huntCts = new CancellationTokenSource();
+
+                PeriodicCheckAsync(_huntCts.Token).Forget();
+            }
+        }
+
         private void OnDetectTarget(IHuntTarget target)
         {
+            if (!_isCanHunt)
+            {
+                return;
+            }
+
             CancelHuntProcess();
             _huntCts = new CancellationTokenSource();
 
@@ -38,6 +65,8 @@ namespace SotongStudio.Bomber.Gameplay.Enemy.Behaviour
 
         private async UniTaskVoid PeriodicCheckAsync(CancellationToken cancellationToken)
         {
+            if (_currentTarget != null) { return; }
+
             while (_currentTarget != null ||
                    !_huntCts.IsCancellationRequested)
             {
