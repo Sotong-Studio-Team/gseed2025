adf53623e429d24682cdc394b8d041834642b9bd
Vindrax-Dev <159908831+Vindrax-Dev@users.noreply.github.com>
2025-08-03 15:58:31 +0700

    adjust shader

---
+++ Assets/Script/ShaderController/ShaderController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShaderController 
+{
+    public const float DefaultDissolveTime = 0.5f;
+    public const string DissolveAmountProperty = "_DissolveAmount";
+    public const string VerticalDissolveAmountProperty = "_VerticalDissolveAmount";
+
+    public static IEnumerator Vanish(Material[] materials)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < DefaultDissolveTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float lerpedDissolve = Mathf.Lerp(0.290f, 1f, elapsedTime / DefaultDissolveTime);
+            float lerpedVerticalDissolve = Mathf.Lerp(0.290f, 1.1f, elapsedTime / DefaultDissolveTime);
+
+            foreach (var mat in materials)
+            {
+                if (mat != null)
+                {
+                    mat.SetFloat(DissolveAmountProperty, lerpedDissolve);
+                    mat.SetFloat(VerticalDissolveAmountProperty, lerpedVerticalDissolve);
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
